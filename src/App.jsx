c2d28ed025b2f47f20dd816da85e33f7bb6cfd13
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-05-28 18:27:45 -0300

    add structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
 import './App.css'
 
 function App() {
 
   return (
-    <div className='App'>
-      <h1>React Multistep</h1>
+    <div className='app'>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto.</p>
+        <div className="form-container">
+          <p>etapas</p>
+          <form>
+            <div className="inputs-container"></div>
+            <div className="actions">
+              <button type='button'>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+              <button type='submit'>
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
