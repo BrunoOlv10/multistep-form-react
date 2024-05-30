52bdca23f5c500ea84a25590f3921128f78c8f3a
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-05-29 22:34:30 -0300

    add next and previous

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <Thanks />
   ]
 
-  const {currentStep, currentComponent} = useForm(formComponents)
+  const {currentStep, currentComponent, changeStep} = useForm(formComponents)
 
   return (
     <div className='app'>
@@ -24,10 +24,10 @@ function App() {
         <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto.</p>
         <div className="form-container">
           <p>etapas</p>
-          <form>
+          <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
             <div className="inputs-container">{currentComponent}</div>
             <div className="actions">
-              <button type='button'>
+              <button type='button' onClick={() => changeStep(currentStep - 1)}>
                 <GrFormPrevious />
                 <span>Voltar</span>
               </button>
